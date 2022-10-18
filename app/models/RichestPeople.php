e2a723992f18eef56a457c960d5ef91ca339af4a
Saad332556 <332556@student.mboutrecht.nl>
2022-10-18 09:59:26 +0200

    nieuwe classes etc

---
+++ app/models/RichestPeople.php
@@ -0,0 +1,29 @@
+<?php
+
+class RichestPeople
+{
+  
+  private $db;
+
+  
+  public function __construct()
+  {
+    $this->db = new Database();
+  }
+
+  
+  public function getRichestPeople()
+  {
+    $this->db->query("SELECT * FROM richestpeople ORDER BY Networth DESC;");
+    $result = $this->db->resultSet();
+    return $result;
+  }
+
+  
+  public function deleteRichestPerson($id)
+  {
+    $this->db->query("DELETE FROM richestpeople WHERE id = :id");
+    $this->db->bind("id", $id, PDO::PARAM_INT);
+    return $this->db->execute();
+  }
+}
