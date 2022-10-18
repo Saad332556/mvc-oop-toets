3bf684b8dc048e4e3f40664c7af17e009623905c
Saad332556 <332556@student.mboutrecht.nl>
2022-10-18 08:52:30 +0200

    eerste commit

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mvc-oop-toets.com');
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+    
+    define('URLROOT', 'http://mvc-oop-toets.com');
+
+    define('SITENAME', 'mvc-framework-met-php');
